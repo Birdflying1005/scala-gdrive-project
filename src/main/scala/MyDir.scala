589c09a45a272e75fe8b0e97be60bc2538000ac9
fma1 <fma1@terpmail.umd.edu>
2017-11-19 20:00:59 -0500

    Added logic for creation of directory structure

--- src/main/scala/MyDir.scala
+++ src/main/scala/MyDir.scala
@@ -1,3 +1,10 @@
-class MyDir {
+import scala.collection.mutable.ListBuffer
+import com.google.api.services.drive.model.{File => GFile}
 
+class MyDir (val name: String, val id: String, val childFiles: List[GFile]) {
+  val childDirs = ListBuffer[MyDir]()
+
+  override def toString: String = {
+    s"MyDir($name, $id, " + childDirs.toString() + ")"
+  }
 }
--- src/main/scala/MyDrive.scala
+++ src/main/scala/MyDrive.scala
@@ -6,10 +6,13 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
 import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.client.util.store.FileDataStoreFactory
 import com.google.api.services.drive.{Drive, DriveScopes}
+import com.google.api.services.drive.model.{File => GFile}
 
 import scala.collection.JavaConverters._
 import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.Queue
 //import util.Try
+
 
 object MyDrive {
   private val APPLICATION_NAME = "scala-gdrive-project"
@@ -38,20 +41,151 @@ object MyDrive {
       .build()
   }
 
+  def getDirectoryStructure(service: com.google.api.services.drive.Drive): MyDir = {
+    val totalList = ListBuffer[GFile]()
+    var pageToken = ""
+
+    do {
+      val result = service.files.list
+        .setPageSize(1000)
+        .setPageToken(pageToken)
+        .setFields("nextPageToken, files(id, name, parents, mimeType)")
+        .execute()
+      totalList ++= result.getFiles.asScala
+      pageToken = result.getNextPageToken
+      println(pageToken)
+    } while (pageToken != null)
+
+    println("Done processing information")
+
+    val files = totalList.toList
+
+    val dirIdToGFileMap = scala.collection.mutable.Map[String, GFile]()
+    val dirIdToDirsMap = scala.collection.mutable.Map[String, ListBuffer[String]]()
+
+    // Add each folder to map such that (Directory ID -> GFile)
+    files.filter(_.getMimeType == "application/vnd.google-apps.folder").foreach(file => {dirIdToGFileMap += (file.getId -> file)})
+
+    // Add each folder (directory ID) to the list of directory IDs for its parent
+    files.filter(_.getMimeType == "application/vnd.google-apps.folder").foreach(file => {
+      val parentsOpt = Option(file.getParents.asScala)
+
+      parentsOpt match {
+        //case Some(parents) => println(s"mimeType: $mimeType, parents: $parents")
+        case Some(parents) => {
+          val childDirId = file.getId
+
+          parents.foreach(parent => {
+            if (!dirIdToDirsMap.contains(parent)) {
+              dirIdToDirsMap += (parent -> new ListBuffer[String]())
+            }
+
+            val dirs = dirIdToDirsMap(parent)
+            dirs += childDirId
+          })
+        }
+        case None => ()
+      }
+    })
+
+    val dirIdToGFilesListMap = scala.collection.mutable.Map[String, ListBuffer[GFile]]()
+
+    files.filterNot(_.getMimeType == "application/vnd.google-apps.folder").foreach(file => {
+      val parentsOpt = Option(file.getParents.asScala)
+
+      parentsOpt match {
+        case Some(parents) => {
+          parents.foreach(parent => {
+            if (!dirIdToGFilesListMap.contains(parent)) {
+              dirIdToGFilesListMap += (parent -> new ListBuffer[GFile]())
+            }
+
+            val dirs = dirIdToGFilesListMap(parent)
+            dirs += file
+          })
+        }
+        case None => ()
+      }
+    })
+
+    val dirIdQueue = Queue[String]()
+    val rootDirId = service.files().get("root").execute().getId
+    val rootChildDirIds = dirIdToDirsMap(rootDirId)
+    val rootDir: MyDir = new MyDir("root", rootDirId, dirIdToGFilesListMap(rootDirId).toList)
+    val dirIdToMyDir = scala.collection.mutable.Map[String, MyDir]()
+
+    dirIdToMyDir += rootDirId -> rootDir
+
+    Option(rootChildDirIds).foreach(x => {dirIdQueue ++= x})
+
+    // This is based on assumption that the directory structure follows a tree-like structure
+    // This probably won't work for loops
+    while (!dirIdQueue.isEmpty) {
+      val dirId = dirIdQueue.dequeue()
+      val gfile = dirIdToGFileMap(dirId)
+
+      // Create new MyDir, add it to map, add it parent MyDir's childDirs
+      val newMyDir = new MyDir(gfile.getName, gfile.getId, util.Try(dirIdToGFilesListMap(dirId).toList).getOrElse(null))
+      dirIdToMyDir += dirId -> newMyDir
+
+      // Add children to queue
+      util.Try(dirIdToDirsMap(dirId)) match {
+        case util.Success(lstBuf) => dirIdQueue ++= lstBuf
+        case util.Failure(_) => ()
+      }
+
+    }
+
+    dirIdToMyDir.keys.filterNot(_.equals(rootDirId)).foreach(dirId => {
+      Option(dirIdToGFileMap(dirId).getParents).foreach(_.forEach(parent => {dirIdToMyDir(parent).childDirs += dirIdToMyDir(dirId)}))
+    })
+
+    rootDir
+  }
+
   def main(args: Array[String]): Unit = {
     val service = getDriveService
-    val result = service.files.list
-      .setPageSize(10)
-      .setFields("nextPageToken, files(id, name)")
-      .execute()
-    var files = result.getFiles
 
-    println(files.get(0).getClass)
-    println("Files: ")
+    val rootMyDir = getDirectoryStructure(service)
+
+    println(rootMyDir)
+
+    /*
+    files.forEach(file => {
+      val parentsOpt = Option(file.getParents.asScala)
+      val mimeType = file.getMimeType
+      //if (mimeType == "application/vnd.google-apps.folder") {
+        parentsOpt match {
+          case Some(parents) => println(s"mimeType: $mimeType, parents: $parents")
+          case None => ()
+        }
+      //}
+    })
+    */
+
+    /*
+    */
+
+    //println(files.get(0).getClass)
+    //println("Files: ")
+    /*
     files.forEach(file => {
       val name = file.getName
       val id = file.getId
       println(s"$name ($id)\n")
     })
+    */
+
+    /*
+    val filenameList = ListBuffer[String]()
+
+    totalList.foreach(file => {
+      filenameList += file.getName
+    })
+
+    val fivesList = filenameList.sorted.grouped(5)
+
+    fivesList.foreach(println(_))
+    */
   }
 }
